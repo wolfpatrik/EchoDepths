40745fa6940fdca762eb9decc873bb4c73cfd973
wolfpatrik <143280004+wolfpatrik@users.noreply.github.com>
2025-09-26 17:18:09 +0200

    Added player movement script

--- Test.cs
+++
@@ -1,6 +0,0 @@
-using Godot;
-
-public partial class Test : Node
-{
-    public override void _Ready() => GD.Print("Na, akkor kezdjünk neki!");
-}
---
+++ scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+public partial class PlayerMovement : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		//TODO: Use camera direction to move relative to camera
+
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
